22921559a790da2fdf140ca1c66f9fd4f0f2e21c
Raja Singh <rajasingh9721@gmail.com>
2020-09-13 20:20:51 +0530

    Updates

---
+++ precache-manifest.869260fd592141c8ba0b088976bffb8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b37cdd96d7845831a81045a625ab59",
+    "url": "/React-todo-app-project/index.html"
+  },
+  {
+    "revision": "85da552cf4e225a75306",
+    "url": "/React-todo-app-project/static/css/main.2a632ff2.chunk.css"
+  },
+  {
+    "revision": "8e8a83dac51522465c15",
+    "url": "/React-todo-app-project/static/js/2.55c3af24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-app-project/static/js/2.55c3af24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85da552cf4e225a75306",
+    "url": "/React-todo-app-project/static/js/main.a2d1a0bf.chunk.js"
+  },
+  {
+    "revision": "c702d98f18d07c4bb9b9",
+    "url": "/React-todo-app-project/static/js/runtime-main.22b4f537.js"
+  }
+]);
